0aad86a61f9987e5f1036bb8022c0969272b206c
van le <vanlecs09@gmail.com>
2018-11-16 20:54:48 +0700

    Update project structure Add StartScreen

---
+++ Assets/RunnerGame/Scripts/App.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnuGames;
+
+public class App : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UIMan.Instance.ShowScreen<UIStartScreen>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
